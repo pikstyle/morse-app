5266469e1161e5f2cead09ef3776c5559c2237f3
pikstyle <pro.pikstyle@gmail.com>
2026-01-10 20:00:27 -0500

    Ajout de l'interface de base

--- morse-app/ContentView.swift
+++ morse-app/ContentView.swift
@@ -8,14 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var textInput: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            
+            Color(red: 0.1, green: 0.1, blue: 0.1)
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                Spacer()
+                
+                Text("Text To Morse")
+                    .font(.largeTitle)
+                
+                Spacer()
+                
+                TextEditor(text: $textInput)
+                    .foregroundColor(.white)
+                    .scrollContentBackground(.hidden)
+                    .frame(height: 120)
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 12)
+                            .fill(Color(red: 0.1, green: 0.1, blue: 0.1))
+                    )
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(Color.white, lineWidth: 1)
+                    )
+                    .padding(.horizontal)
+                
+                Spacer()
+                
+                Button {
+                    // action
+                } label: {
+                    HStack {
+                        Image(systemName: "flashlight.on.fill")
+                        Text("Convert")
+                    }
+                    .padding()
+                }
+                .buttonStyle(.bordered)
+                .tint(Color.red)
+                .font(.title)
+                Spacer()
+                
+            }
+            .padding()
+            .foregroundColor(Color.white)
         }
-        .padding()
     }
 }
 
