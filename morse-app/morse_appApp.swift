e5fd63fc6184e4100aac802eccbd99722e8c9126
pikstyle <pro.pikstyle@gmail.com>
2026-01-10 19:02:21 -0500

    Initial commit

---
+++ morse-app/morse_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  morse_appApp.swift
+//  morse-app
+//
+//  Created by Simon Mounier on 2026-01-10.
+//
+
+import SwiftUI
+
+@main
+struct morse_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
