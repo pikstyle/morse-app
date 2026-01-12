9721482614f71912f6f3eaf024566af1dda30ac2
pikstyle <pro.pikstyle@gmail.com>
2026-01-11 21:10:18 -0500

    Ajout des fonctions pour convertir le texte en code morse

--- morse-app/ContentView.swift
+++ morse-app/ContentView.swift
@@ -9,19 +9,44 @@ import SwiftUI
 import AVFoundation
 
 struct ContentView: View {
-    
     @State private var textInput: String = ""
-    let device = AVCaptureDevice.default(for: .video)
-    
-    var body: some View {
+    let flashlight = AVCaptureDevice.default(for: .video)
+    let morseDictionnary: [String:String] = [
+        "a": ".-",
+        "b": "-...",
+        "c": "-.-.",
+        "d": "-..",
+        "e": ".",
+        "f": "..-.",
+        "g": "--.",
+        "h": "....",
+        "i": "..",
+        "j": ".---",
+        "k": "-.-",
+        "l": ".-..",
+        "m": "--",
+        "n": "-.",
+        "o": "---",
+        "p": ".--.",
+        "q": "--.-",
+        "r": ".-.",
+        "s": "...",
+        "t": "-",
+        "u": "..-",
+        "v": "...-",
+        "w": ".--",
+        "x": "-..-",
+        "y": "-.--",
+        "z": "--.."
+    ]
+    @State private var morseCode = [String]()
         
+    var body: some View {
         ZStack {
-            
             Color(red: 0.1, green: 0.1, blue: 0.1)
                 .ignoresSafeArea()
             
             VStack {
-                
                 Spacer()
                 
                 Text("Text To Morse")
@@ -30,24 +55,27 @@ struct ContentView: View {
                 Spacer()
                 
                 TextEditor(text: $textInput)
-                    .foregroundColor(.white)
+                    .frame(height: 100)
                     .scrollContentBackground(.hidden)
-                    .frame(height: 120)
+                    .background(Color(red: 0.1, green: 0.1, blue: 0.1))
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
                     .padding()
-                    .background(
-                        RoundedRectangle(cornerRadius: 12)
-                            .fill(Color(red: 0.1, green: 0.1, blue: 0.1))
-                    )
                     .overlay(
-                        RoundedRectangle(cornerRadius: 12)
-                            .stroke(Color.white, lineWidth: 1)
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.white, lineWidth: 2)
                     )
-                    .padding(.horizontal)
+                    .padding()
+                
+                Spacer()
+                
+                Text("Text en morse : \(morseCode.joined(separator: " "))")
                 
                 Spacer()
                 
                 Button {
-                    if device?.torchMode == .on {
+                    convert()
+                    if flashlight?.torchMode == .on {
                         toggleTorch(on: false)
                     } else {
                         toggleTorch(on: true)
@@ -60,33 +88,61 @@ struct ContentView: View {
                     .padding()
                 }
                 .buttonStyle(.bordered)
-                .tint(Color.red)
+                .tint(.red)
                 .font(.title)
+                                                
                 Spacer()
                 
             }
             .padding()
-            .foregroundColor(Color.white)
+            .foregroundColor(.white)
         }
     }
     
-    // Fonction qui permet d'aller ou éteindre la lampe torche
+    // Flashlight Control
     
     func toggleTorch(on: Bool) {
         guard let device = AVCaptureDevice.default(for: .video) else { return }
+        
         if device.hasTorch {
             do {
                 try device.lockForConfiguration()
-                if on {
-                    device.torchMode = .on
-                } else {
-                    device.torchMode = .off
-                }
+                device.torchMode = on ? .on : .off
                 device.unlockForConfiguration()
             } catch {
                 print("Torch could not be used")
             }
         }
+    }
+    
+    // Convert text to an array of letters
+    
+    func convertTextToLetters(text: String) -> [String] {
+        var letterArray = [String]()
+        for letter in text {
+            letterArray.append(letter.lowercased())
+        }
+        return letterArray
+    }
+    
+    // Convert the array of letters to an array of string, where each string represent the letter by it's morse representation
+    
+    func convertLettersToMorse(letters: [String]) -> [String] {
+        var morseArray = [String]()
+        for letter in letters {
+            for (key, value) in morseDictionnary where key == letter {
+                morseArray.append(value)
+            }
+        }
+        return morseArray
+    }
+    
+    // Return the final conversion
+    
+    func convert() {
+        let letters = convertTextToLetters(text: textInput)
+        morseCode = convertLettersToMorse(letters: letters)
+        print(morseCode)
     }
 }
 
